625c79889af2a93c137c80691d5b8f9e6f58a149
lech.piechota <lech.piechota@ocado.com>
2024-11-16 13:14:07 +0100

    solution for day1 part2

--- solutions/day01/solution_day01.py
+++ solutions/day01/solution_day01.py
@@ -1,3 +1,5 @@
+import re
+
 def get_number(string):
     for i in string:
         if i.isdigit():
@@ -17,3 +19,55 @@ def get_sum(strings):
 
 def part1(strings):
     return get_sum(strings)
+
+
+def get_number_part2(string):
+    # Define number map in descending order of word length
+    glued_numbers = {
+        "eightwo": "eighttwo",
+        "eighthree" : "eightthree",
+        "oneight": "oneeight",
+        "threeight": "threeeight",
+        "fiveight": "fiveeight",
+        "sevenine": "sevennine",
+        "nineight": "nineeight",
+        "twone": "twoone",
+    }
+
+    # Replace glued numbers with their correct spelling
+    for glued, correct in glued_numbers.items():
+        string = string.replace(glued, correct)
+
+    # Map of spelled-out numbers to digits
+    number_map = {
+        "one": 1, "two": 2, "three": 3, "four": 4,
+        "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9
+    }
+
+    # List to store extracted digits
+    digits = []
+
+    # Use regex to find potential matches (digits or spelled-out words)
+    matches = re.finditer(r"\d|one|two|three|four|five|six|seven|eight|nine", string)
+
+    for match in matches:
+        token = match.group()
+        if token.isdigit():
+            digits.append(int(token))
+        elif token in number_map:
+            digits.append(number_map[token])
+
+    if digits:
+        return int(str(digits[0])+ str(digits[-1]))
+    return None, None  # Handle cases where no digits are found
+
+def get_sum_part2(strings):
+    _sum = 0
+    for string in strings:
+        number = get_number_part2(string)
+        if number:
+            _sum += number
+    return _sum
+
+def part2(strings):
+    return get_sum_part2(strings)
--- tests/day01/test_solution_day01.py
+++ tests/day01/test_solution_day01.py
@@ -1,7 +1,7 @@
 import unittest
 
 from solutions.common.readers import read_strings
-from solutions.day01.solution_day01 import get_number, get_sum, part1
+from solutions.day01.solution_day01 import get_number, get_sum, part1, get_sum_part2, get_number_part2, part2
 
 
 class TestDay01Unit(unittest.TestCase):
@@ -14,14 +14,34 @@ class TestDay01Unit(unittest.TestCase):
     def test_get_sum(self):
         self.assertEqual(get_sum(["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]), 142)
 
+    def test_get_number_part2(self):
+        self.assertEqual(get_number_part2("two1nine"), 29)
+        self.assertEqual(get_number_part2("eightwothree"), 83)
+        self.assertEqual(get_number_part2("abcone2threexyz"), 13)
+        self.assertEqual(get_number_part2("xtwone3four"), 24)
+        self.assertEqual(get_number_part2("4nineeightseven2"), 42)
+        self.assertEqual(get_number_part2("zoneight234"), 14)
+        self.assertEqual(get_number_part2("7pqrstsixteen"), 76)
+        self.assertEqual(get_number_part2("3xtwone"), 31)
+
 class TestDay01Solution(unittest.TestCase):
-    def test_solution(self):
+
+    def test_solution_part1(self):
         input_test_1 = read_strings("test1.txt")
         self.assertEqual(part1(input_test_1), 142)
 
-    def test_result_on_real_input(self):
-        input_1 = read_strings("input1.txt")
-        print(part1(input_1))
+    def test_result_on_real_input_part1(self):
+        _input = read_strings("input.txt")
+        print(part1(_input))
+
+    def test_solution_part2(self):
+        input_test_2 = read_strings("test2.txt")
+        self.assertEqual(part2(input_test_2), 281)
+
+    def test_result_on_real_input_part2(self):
+        _input = read_strings("input.txt")
+        print(part2(_input))
+        # 53500 too low
 
 if __name__ == '__main__':
     unittest.main()
